522363bead79dd762e11c8bfb69efbe17c707b4b
Mikko Kujala <m.kujala@live.com>
2024-12-12 14:20:07 +0200

    cors allowed from anywhere

--- raamattu-be/src/main.rs
+++ raamattu-be/src/main.rs
@@ -3,8 +3,14 @@ mod error;
 mod handlers;
 mod state;
 
-use axum::routing::get;
-use axum::Router;
+use axum::extract::State;
+use axum::{http::Method, routing::get};
+use axum::{Extension, Router};
+
+use tower::{Service, ServiceBuilder, ServiceExt};
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace::TraceLayer;
+
 use handlers::get_book_list;
 use state::*;
 
@@ -15,12 +21,22 @@ async fn main() {
         return;
     }
 
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET])
+        // allow requests from any origin
+        .allow_origin(Any);
+
+    let be_state = BackendState::new(conn_str.expect("RAAMATTU_PG is undefined").as_str()).await;
+
     let app = Router::new()
         .route("/", get(|| async { "Hello" }))
         .route("/book-list/by-translation/:translation", get(get_book_list))
-        .with_state(BackendState::new(conn_str.unwrap().as_str()).await);
+        .layer(cors)
+        .layer(TraceLayer::new_for_http())
+        .with_state(be_state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await.expect("LALA");
 }
